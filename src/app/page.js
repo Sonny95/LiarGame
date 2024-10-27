db1b7a5ca7e8d7fafb8ffa3d75220dfc41272edb
Sonny95 <thswlgml9598@gmail.com>
2024-10-27 22:45:53 +1100

    menu changed

--- src/app/page.js
+++ src/app/page.js
@@ -2,31 +2,34 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-1/3 h-3/4 bg-gray-300 ">
+    <div className="w-full h-screen flex items-center justify-center ">
+      <div className="w-1/3 h-3/4 bg-gray-800">
         {/* top */}
-        <div className="text-2xl w-full h-1/6 bg-gray-400 flex items-center justify-center">
+        <div className="text-2xl w-full h-1/6 flex items-center justify-center">
           <div>Your Title is</div>
           <div className="ml-2 font-bold">Title</div>
-          <button className="ml-4 w-28 h-8 bg-gray-500 rounded-xl text-base">Change Title</button>
+          <button className="ml-4 w-28 h-8 rounded-xl text-base bg-blue-500">Change Title</button>
         </div>
 
         {/* set up */}
-
-        <div className="w-1/3 h-3/4 bg-gray-300 ">
-          {/* players */}
-          <div className="w-full h-1/4 bg-gray-400 ">
-            <div>players</div>
-            <button className="w-8 h-8 bg-yellow-300 rounded-full "> - </button>
-            <div className="w-8 h-8 bg-white"></div>
-            <button className="w-8 h-8 bg-yellow-300 rounded-full "> + </button>
-          </div>
-          {/* choose liar */}
-          <div className="w-full h-1/4 bg-gray-400 ">
-            <div>Liars</div>
-            <button className="w-8 h-8 bg-yellow-300 rounded-full "> - </button>
-            <div className="w-8 h-8 bg-white"></div>
-            <button className="w-8 h-8 bg-yellow-300 rounded-full "> + </button>
+        <div className=" w-full h-3/4 flex items-cetner justify-center font-bold">
+          <div className="w-1/3 h-3/4 flex flex-col items-center justify-center">
+            {/* players */}
+            <div className="w-full h-1/4 flex items-center justify-between">
+              <div>players</div>
+              <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> - </button>
+              <div className="w-8 h-8 bg-white flex items-center justify-center text-black">3</div>
+              <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> + </button>
+            </div>
+            {/* choose liar */}
+            <div className="w-full h-1/4 flex items-center justify-between">
+              <div>Liars</div>
+              <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> - </button>
+              <div className="w-8 h-8 bg-white flex items-center justify-center text-black">1</div>
+              <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> + </button>
+            </div>{" "}
+            {/* start button */}
+            <button className="w-28 h-8 bg-blue-500 rounded-xl text-base mt-12"> PLAY</button>
           </div>
         </div>
       </div>
