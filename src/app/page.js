4a2e0e1847a094015402f9d4ebd94396fc2f6412
Sonny95 <thswlgml9598@gmail.com>
2024-10-28 23:13:05 +1100

    first page done

--- src/app/page.js
+++ src/app/page.js
@@ -13,17 +13,21 @@ export default function Home() {
 
         {/* set up */}
         <div className=" w-full h-3/4 flex items-cetner justify-center font-bold">
-          <div className="w-1/3 h-3/4 flex flex-col items-center justify-center">
+          <div className="w-2/3 h-3/4 flex flex-col items-center justify-center">
             {/* players */}
-            <div className="w-full h-1/4 flex items-center justify-between">
-              <div>players</div>
+            <div className="w-2/3 h-1/4 flex items-center justify-between">
+              <div className="w-1/2 flex items-center justify-center">
+                <div>players</div>
+              </div>
               <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> - </button>
               <div className="w-8 h-8 bg-white flex items-center justify-center text-black">3</div>
               <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> + </button>
             </div>
             {/* choose liar */}
-            <div className="w-full h-1/4 flex items-center justify-between">
-              <div>Liars</div>
+            <div className="w-2/3 l h-1/4 flex items-center justify-between">
+              <div className="w-1/2 flex items-center justify-center">
+                <div>Liars</div>
+              </div>
               <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> - </button>
               <div className="w-8 h-8 bg-white flex items-center justify-center text-black">1</div>
               <button className="w-8 h-8 bg-gray-300 rounded-full text-black"> + </button>
