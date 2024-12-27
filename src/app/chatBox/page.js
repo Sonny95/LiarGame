e033e0ca22b7f1bae9e312276cb58b64c5d6a95f
Sonny95 <thswlgml9598@gmail.com>
2024-12-27 23:08:42 +1100

    title

--- src/app/chatBox/page.js
+++ src/app/chatBox/page.js
@@ -8,7 +8,7 @@ function ChatBox() {
       <div className="w-full h-3/4 max-w-md p-4 bg-gray-800 text-white rounded-lg transition-all duration-500 ease-in-out">
         <div className="text-2xl w-full h-1/6 flex items-center justify-center">
           <div>You are a</div>
-          <div className="ml-2 font-bold">"Liar"</div>
+          <div className="ml-2 font-bold">"A"</div>
         </div>
         <div
           className={`overflow-y-scroll scrollbar-thin scrollbar-thumb-white mb-4 border border-gray-600 p-2 rounded-lg flex flex-col h-96`}
